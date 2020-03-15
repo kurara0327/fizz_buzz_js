290f4cad2f34fd608e365e4836ad2948fcdec9b2
Hajime_kurakata <h.kurara080327@gmail.com>
2020-03-15 23:26:33 +0900

    first commit

---
+++ src/fizz_buzz.js
@@ -0,0 +1,15 @@
+const readline = require('readline-sync');
+
+console.log("Game Start!");
+
+const answer = readline.question("Input Number? ");
+
+for (var i = 1; i <= answer; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("Fizz Buzz!");
+  }else if (i % 3 == 0) {
+    console.log("Fizz");
+  }else if (i % 5 == 0) {
+    console.log("Buzz");
+  }
+}
